677f067c46e25b3525a1baccd96a63fe5cd6ab15
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-06 17:30:05 +0530

    Add Site Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-emerald-500">App</h1>
+    <div className="min-h-screen font-default bg-main grid items-center py-8">
+      <div className="max-w-lg w-10/12 mx-auto">
+        <h1 className="text-2xl text-center font-bold text-white bg-display">
+          Calculator
+        </h1>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,78 @@ import Themer from 'tailwindcss-themer';
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   darkMode: 'class',
   plugins: [
     Themer({
-      defaultTheme: {},
+      defaultTheme: {
+        extend: {
+          fontFamily: {
+            default: "'League Spartan', sans-serif",
+          },
+          colors: {
+            main: 'hsl(222, 26%, 31%)',
+            keypad: 'hsl(223, 31%, 20%)',
+            display: 'hsl(224, 36%, 15%)',
+            key: {
+              primary: 'hsl(30, 25%, 89%)',
+              primaryShadow: 'hsl(28, 16%, 65%)',
+              secondary: 'hsl(225, 21%, 49%)',
+              secondaryShadow: 'hsl(224, 28%, 35%)',
+              accent: 'hsl(6, 63%, 50%)',
+              accentShadow: 'hsl(6, 70%, 34%)',
+            },
+            text: {
+              primary: 'hsl(221, 14%, 31%)',
+              secondary: 'hsl(221, 14%, 31%)',
+            },
+          },
+        },
+      },
       themes: [
         {
           name: 'light',
+          extend: {
+            colors: {
+              main: 'hsl(0, 0%, 90%)',
+              keypad: 'hsl(0, 5%, 81%)',
+              display: 'hsl(0, 0%, 93%)',
+              key: {
+                primary: 'hsl(45, 7%, 89%)',
+                primaryShadow: 'hsl(35, 11%, 61%)',
+                secondary: 'hsl(185, 42%, 37%)',
+                secondaryShadow: 'hsl(185, 58%, 25%)',
+                accent: 'hsl(25, 98%, 40%)',
+                accentShadow: 'hsl(25, 99%, 27%)',
+              },
+              text: {
+                primary: 'hsl(60, 10%, 19%)',
+                secondary: 'hsl(60, 10%, 19%)',
+              },
+            },
+          },
         },
         {
           name: 'pink',
+          extend: {
+            colors: {
+              main: 'hsl(268, 75%, 9%)',
+              keypad: 'hsl(268, 71%, 12%)',
+              display: 'hsl(268, 71%, 12%)',
+              key: {
+                primary: 'hsl(268, 47%, 21%)',
+                primaryShadow: 'hsl(290, 70%, 36%)',
+                secondary: 'hsl(281, 89%, 26%)',
+                secondaryShadow: 'hsl(285, 91%, 52%)',
+                accent: 'hsl(176, 100%, 44%)',
+                accentShadow: 'hsl(177, 92%, 70%)',
+              },
+              text: {
+                primary: 'hsl(52, 100%, 62%)',
+                secondary: 'hsl(198, 20%, 13%)',
+              },
+            },
+          },
         },
       ],
     }),
